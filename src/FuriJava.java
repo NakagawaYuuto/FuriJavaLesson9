e8ea1e6016b3111112cffbae960ac148d3077360
NakagawaYuuto <github.nakagawa@gmail.com>
2022-11-10 20:05:10 +0900

    繰り返し文　part2

--- src/FuriJava.java
+++ src/FuriJava.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class FuriJava {
-    //Chap3_2_1
     public static void main(String[] args) {
+        //Chap3_2_1
         int money = 30000;
         while( money >= 0 ) {
             System.out.println(money);
@@ -34,6 +36,64 @@ public class FuriJava {
             for (int y = 1; y <= 9; y++) {
                 System.out.println(x + "*" + y  + "=" + x * y);
             }
+        }
+
+        //Chap3_6_1
+        String [] ewsn = {"東", "西", "南", "北"};
+        System.out.println(ewsn[1]);
+
+        //Chap3_6_2
+        String [] ewsn1 = {"東", "西", "南", "北"};
+        ewsn1[0] = "真東";
+        System.out.println(ewsn1[0]);
+
+        String [] ewsn2 = new String[4];
+        ewsn2[0] = "東";
+        ewsn2[1] = "西";
+
+        //Chap3_7_1
+        String [] ewsn3 = {"東", "西", "南", "北"};
+        for(String str : ewsn3){
+            System.out.println(str + "方向");
+        }
+
+        //Chap3_8_1
+        String [] team = {"A", "B", "C", "D", "E"};
+        for(String t1 : team){
+            for (String t2 : team){
+                System.out.println(t1 + "vs" + t2);
+            }
+        }
+
+        //Chap3_8_2
+        String [] team1 = {"A", "B", "C", "D", "E"};
+        for(String t1 : team1){
+            for (String t2 : team1){
+                if (!t1.equals(t2))
+                System.out.println(t1 + "vs" + t2);
+            }
+        }
+
+        String[] team2 = {"A", "B", "C", "D", "E"};
+        for (int i = 0; i < team2.length; i++) {
+            System.out.println(team2[i]);
+        }
+
+        //Chap3_9_1(無限ループ）
+        int num = 0;
+        while (num >= 0){
+            num = num +1;
+            System.out.println(num);
+        }
+
+        String[] grade = {"松", "竹", "梅"};
+        for (String g : grade){
+            System.out.println(g);
+        }
+
+        String[] ewsn4 = {"東", "西", "南", "北"};
+        for (int i = 3; i >= 0; i--) {
+            System.out.println(ewsn4[i] + "方向");
         }
     }
 }
